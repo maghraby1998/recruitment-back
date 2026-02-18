336d2423a174c338b5875945fbd3d8ff75a0f609
maghraby <amaghraby@qurtobasolutions.com>
2026-02-18 09:58:53 +0200

    integrate with openai to generate skills based on the position

--- src/skill/skill.resolver.ts
+++ src/skill/skill.resolver.ts
@@ -3,10 +3,18 @@ import { SkillService } from './skill.service';
 import { CreateSkillDto } from './dtos/create-skill.dto';
 import { Auth } from 'src/decorators/auth.decorator';
 import { User } from 'generated/prisma/client';
+import OpenAI from 'openai';
+import { Public } from 'src/decorators/public.decorator';
 
 @Resolver('Skill')
 export class SkillResolver {
   constructor(private skillService: SkillService) {}
+
+  private openai = new OpenAI({
+    apiKey:
+      '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',
+    baseURL: 'https://openrouter.ai/api/v1',
+  });
 
   @Query()
   async getMySkills(@Auth() auth: User) {
@@ -21,5 +29,31 @@ export class SkillResolver {
   @Mutation()
   async createSkill(@Args('input') input: CreateSkillDto, @Auth() auth: User) {
     return this.skillService.createSkill(input, auth.id);
+  }
+
+  @Public()
+  @Query()
+  async generateSkills(@Args('position') position: string) {
+    const prompt = `
+    Generate a list of professional skills required for the position: "${position}".
+
+    Return the response strictly as JSON in this format:
+    {
+      "skills": ["skill1", "skill2", "skill3"]
+    }
+    `;
+
+    const response = await this.openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [
+        { role: 'system', content: 'You are an HR recruitment expert.' },
+        { role: 'user', content: prompt },
+      ],
+      temperature: 0.3,
+    });
+
+    return (
+      JSON.parse(response.choices[0].message.content ?? '{}')?.skills ?? []
+    );
   }
 }
